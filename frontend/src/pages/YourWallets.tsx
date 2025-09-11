47e6d2174fe003a86dd277ad2e8f33093eeafb01
Scrom <formain337@gmail.com>
2025-09-11 16:00:55 +0530

    readme update

--- frontend/src/pages/YourWallets.tsx
+++ frontend/src/pages/YourWallets.tsx
@@ -95,45 +95,43 @@ const YourWallets = () => {
   // Render
   // ------------------------
   return (
-    <div className="h-full w-full grid grid-rows-[130px_auto] justify-center">
+    <div className="h-full w-full grid grid-rows-[130px_auto] justify-items-center overflow-y-scroll">
       {/* Header with account name and Add Account button */}
-      <div className="mx-20 mt-10">
-        <div className="flex py-3 justify-between gap-20">
-          <h1 className="text-4xl font-extrabold font-poppins mb-5 bg-gradient-to-b from-blue-400 to-blue-700 bg-clip-text text-transparent tracking-tight drop-shadow-[0_0_10px_rgba(0,97,255,0.59)]">
-            {account[Number(accID)].accountName}
-          </h1>
+      <div className="flex w-auto py-3 justify-between gap-20">
+        <h1 className="text-4xl font-extrabold font-poppins mb-5 bg-gradient-to-b from-blue-400 to-blue-700 bg-clip-text text-transparent tracking-tight drop-shadow-[0_0_10px_rgba(0,97,255,0.59)]">
+          {account[Number(accID)].accountName}
+        </h1>
 
-          <button
-            className="h-10 w-auto bg-gradient-to-b from-blue-400 to-blue-700 text-white px-3 rounded-xl text-sm font-bold hover:cursor-pointer hover:transition hover:duration-300 hover:scale-110"
-            onClick={handleOnClickCreateAccount}
-          >
-            ADD ACCOUNT
-          </button>
-        </div>
+        <button
+          className="h-10 w-auto bg-gradient-to-b from-blue-400 to-blue-700 text-white px-3 rounded-xl text-sm font-bold hover:cursor-pointer hover:transition hover:duration-300 hover:scale-110"
+          onClick={handleOnClickCreateAccount}
+        >
+          ADD ACCOUNT
+        </button>
       </div>
 
       {/* Wallet Cards Section */}
-      <div className="h-full w-full gap-10 overflow-y-scroll scroll-smooth">
+      <div className="gap-10 scroll-smooth w-[350px] sm:w-[500px] md:w-[600px] lg:w-[1000px]">
         {account.map(
           (acc, index) =>
             Number(accID) === acc.accountNumber &&
             Object.entries(acc.accountDetails).map(([walletType, walletDetails]) => (
-              <div key={`${index}-${walletType}`} className="relative h-auto w-auto max-w-[1200px] overflow-hidden mx-20 z-50 mb-10">
+              <div key={`${index}-${walletType}`} className="relative h-auto w-auto overflow-hidden z-50 mb-10">
                 {/* Background Blur Effect */}
                 <div className="absolute h-[200px] w-full bg-blue-500 rounded-full blur-[170px] left-1/2 transform -translate-x-1/2 top-1/3 -translate-y-1/2 z-0"></div>
 
                 {/* Wallet Card */}
-                <div className="h-auto min-w-[1000px] rounded-2xl backdrop-blur-xl border border-blue-400/30 shadow-lg shadow-black/40 p-10">
+                <div className="h-auto rounded-2xl backdrop-blur-xl border border-blue-400/30 shadow-lg shadow-black/40 p-5 sm:p-10">
                   {/* Wallet Name and Icon with Add Balance btn */}
                   <div className="flex justify-between items-center mb-5">
                     {/* Wallet Name and Icon */}
-                    <div className="text-2xl text-[#dcdcdc] font-bold px-3 flex items-center gap-3">
-                      <img src={walletDetails.walletIconLocation} alt="" className="h-10 w-10" />
+                    <div className="text-l md:text-2xl text-[#dcdcdc] font-bold px-3 flex items-center gap-2 md:gap-3">
+                      <img src={walletDetails.walletIconLocation} alt="" className="h-7 w-7 md:h-10 md:w-10" />
                       {walletDetails.walletName}
                     </div>
                     {selectedComponent !== walletDetails.privateKey && (
                       <button
-                        className="h-8 w-auto bg-blue-500 text-white px-3 rounded-xl text-sm font-medium transition duration-300 transform hover:scale-110 cursor-pointer"
+                        className="h-8 w-auto bg-blue-500 text-white px-3 rounded-xl text-[10px] md:text-sm font-medium transition duration-300 transform hover:scale-110 cursor-pointer"
                         onClick={() => {
                           setSelectedComponent(walletDetails.privateKey);
                         }}
@@ -180,14 +178,16 @@ const WalletCredentials = ({ walletDetails, selectedComponent }: { walletDetails
       const publicKey = new PublicKey(walletDetails.publicKey);
       const airdropSignature = await connection.requestAirdrop(publicKey, 1 * LAMPORTS_PER_SOL);
       const latestBlockhash = await connection.getLatestBlockhash();
-    
-    const confirmation = await connection.confirmTransaction({
-      signature: airdropSignature,
-      blockhash: latestBlockhash.blockhash,
-      lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
-    }, "confirmed");
-    console.log(confirmation)
 
+      const confirmation = await connection.confirmTransaction(
+        {
+          signature: airdropSignature,
+          blockhash: latestBlockhash.blockhash,
+          lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+        },
+        "confirmed"
+      );
+      console.log(confirmation);
     } catch (err) {
       console.error("Airdrop failed:", err);
     }
@@ -212,25 +212,25 @@ const WalletCredentials = ({ walletDetails, selectedComponent }: { walletDetails
       </div>
     </div>
   ) : (
-    <div className="h-auto border border-blue-400/30 rounded-2xl text-[#dcdcdc] bg-blue-500/15 flex flex-col justify-center p-10 gap-10">
-      <h1 className="font-medium text-xl font-poppins text-white/80">Wallet Credentials</h1>
+    <div className="h-auto border border-blue-400/30 rounded-2xl text-[#dcdcdc] bg-blue-500/15 flex flex-col justify-center p-5 md:p-10 gap-5 md:gap-10">
+      <h1 className="font-medium text-sm md:text-xl font-poppins text-white/80">Wallet Credentials</h1>
       <div className="flex flex-col gap-4">
         {/* Public Key */}
         <div>
-          <div className="flex justify-between px-1">
-            <h1 className="text-sm text-white/80 mb-2">PUBLIC KEY</h1>
-            <h1 className="text-sm text-white/80 mb-2">COPY</h1>
+          <div className="flex justify-between px-1 md:text-sm text-[10px]">
+            <h1 className="text-white/80 mb-2">PUBLIC KEY</h1>
+            <h1 className="text-white/80 mb-2">COPY</h1>
           </div>
-          <h1 className="text-md text-[#ebebebcc] p-2 px-4 rounded-[10px] bg-[#06122c] font-roboto">{walletDetails.publicKey}</h1>
+          <h1 className="text-md text-[#ebebebcc] p-2 px-4 rounded-[10px] bg-[#06122c] font-roboto">{walletDetails.publicKey.substring(0, 7)}...</h1>
         </div>
 
         {/* Private Key */}
         <div>
-          <div className="flex justify-between px-1">
-            <h1 className="text-sm text-white/80 mb-2">PRIVATE KEY</h1>
-            <h1 className="text-sm text-white/80 mb-2">COPY</h1>
+          <div className="flex justify-between px-1 md:text-sm text-[10px]">
+            <h1 className="text-white/80 sm:mb-2">PRIVATE KEY</h1>
+            <h1 className="text-white/80 mb-2">COPY</h1>
           </div>
-          <h1 className="text-md text-[#ebebebcc] p-2 px-4 rounded-[10px] bg-[#06122c] font-roboto">{walletDetails.privateKey}</h1>
+          <h1 className="text-md text-[#ebebebcc] p-2 px-4 rounded-[10px] bg-[#06122c] font-roboto">{walletDetails.privateKey.substring(0, 7)}...</h1>
         </div>
       </div>
     </div>
